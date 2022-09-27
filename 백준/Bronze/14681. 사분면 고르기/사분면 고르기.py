052d0b9e52e973efd05af7eef39ef2eab09de12d
GrO0vy <89755903+GrO0vy@users.noreply.github.com>
2022-09-27 11:19:01 +0900

    [Bronze V] Title: 사분면 고르기, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.py
@@ -0,0 +1,12 @@
+n1 = int(input())
+n2 = int(input())
+
+if n1<0 and n2 >0:
+    print(2)
+elif n1>0 and n2 >0:
+    print(1)
+elif n1 >0 and n2<0:
+    print(4)
+elif n1<0 and n2 <0:
+    print(3)
+    
