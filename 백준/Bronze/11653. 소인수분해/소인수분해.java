5e7feae3e6b267fb7441d8e4e345b83cb70b35be
GrO0vy <89755903+GrO0vy@users.noreply.github.com>
2023-03-20 00:54:13 +0900

    [Bronze I] Title: 소인수분해, Time: 240 ms, Memory: 17740 KB -BaekjoonHub

---
+++ 백준/Bronze/11653. 소인수분해/소인수분해.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        
+        int k = 2;
+        while(num != 1){
+            if(num%k == 0){
+                System.out.println(k);
+                num/=k;
+            }
+            else k++;
+        }
+        
+        sc.close();
+    }
+}
