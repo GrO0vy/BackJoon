36eb6500acf2eb6ab79307e01ee64fa2864b3751
GrO0vy <89755903+GrO0vy@users.noreply.github.com>
2023-03-29 19:04:04 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 4, Time: 204 ms, Memory: 17612 KB -BaekjoonHub

--- 백준/Bronze/24265. 알고리즘 수업 － 알고리즘의 수행 시간 4/알고리즘 수업 － 알고리즘의 수행 시간 4.java
+++ 백준/Bronze/24265. 알고리즘 수업 － 알고리즘의 수행 시간 4/알고리즘 수업 － 알고리즘의 수행 시간 4.java
@@ -4,13 +4,8 @@ public class Main{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         long n = sc.nextInt();
-        long minus = 0;
         
-        for(int i = 1; i<n; i++){
-            minus +=i;
-        }
-        
-        System.out.println(n*(n-1)-minus);
+        System.out.println(n*(n-1)/2);
         System.out.println(2);
         
         sc.close();
