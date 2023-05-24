59a147b996fb9aa322b0d3b9c7be07b48528162b
이민철 <89755903+GrO0vy@users.noreply.github.com>
2023-05-24 14:41:04 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.16 ms, Memory: 74.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120853. 컨트롤 제트/컨트롤 제트.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+        String[] numbers = s.split(" ");
+        for(int i=0; i<numbers.length; i++){
+            if(numbers[i].equals("Z")){
+                answer -= Integer.valueOf(numbers[i-1]);
+            }
+            else{
+                answer += Integer.valueOf(numbers[i]);
+            }
+        }
+        return answer;
+    }
+}
