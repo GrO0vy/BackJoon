d423a1002d77d692ecd8ed214310eaf6555ab39f
이민철 <89755903+GrO0vy@users.noreply.github.com>
2023-05-25 17:49:52 +0900

    [level 0] Title: 로그인 성공?, Time: 0.02 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.java
+++ 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.java
@@ -1,22 +1,16 @@
 class Solution {
     public String solution(String[] id_pw, String[][] db) {
-        String answer = "";
         String id = id_pw[0];
         String pw = id_pw[1];
         
-        boolean idExist = false;
-        boolean correctPw = false;
-        
         for(int i=0; i<db.length; i++){
             if(id.equals(db[i][0])){
-                idExist = true;
-                if(pw.equals(db[i][1])) correctPw = true;  
+                if(pw.equals(db[i][1])){
+                    return "login";
+                }
+                else return "wrong pw";
             }
         }
-        if(!idExist) answer = "fail";
-        else if(!correctPw) answer = "wrong pw";
-        else answer = "login";
-        
-        return answer;
+        return "fail";
     }
 }
