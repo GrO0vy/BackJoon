d13091964a641d8bb30d2ec9167e21494c021057
이민철 <89755903+GrO0vy@users.noreply.github.com>
2023-06-05 20:43:39 +0900

    [unrated] Title: 부분 문자열, Time: 0.03 ms, Memory: 76.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181842. 부분 문자열/부분 문자열.java
@@ -0,0 +1,7 @@
+class Solution {
+    public int solution(String str1, String str2) {
+        int answer = 1;
+        if(str2.equals(str2.replace(str1,""))) answer = 0;
+        return answer;
+    }
+}
