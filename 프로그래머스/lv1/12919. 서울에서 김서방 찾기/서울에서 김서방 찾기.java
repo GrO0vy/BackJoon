1fb43ba9680a56b1a39d9711805acce767ce7c6f
이민철 <89755903+GrO0vy@users.noreply.github.com>
2023-06-14 17:00:31 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.93 ms, Memory: 82.2 MB -BaekjoonHub

--- 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
+++ 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
@@ -1,12 +1,8 @@
+import java.util.Arrays;
 class Solution {
     public String solution(String[] seoul) {
-        String answer ="";
-        for(int i = 0; i < seoul.length; i++){
-            if(seoul[i].equals("Kim")) {
-                answer = String.format("김서방은 %d에 있다",i);
-                break;
-            }
-        }
-        return answer;
+        int x = Arrays.asList(seoul).indexOf("Kim");
+        
+        return String.format("김서방은 %d에 있다",x);
     }
 }
