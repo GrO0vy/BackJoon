e80c269a21fbd0fd3439f6ee6d2b077288e160cc
이민철 <89755903+GrO0vy@users.noreply.github.com>
2023-06-14 16:52:23 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.02 ms, Memory: 71.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.java
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -1,21 +1,12 @@
 class Solution {
     public int solution(int num) {
-        int answer = 0;
-        
-        long number = num;
-            
-        while(number != 1){
-            if(answer > 500) {
-                answer = -1;
-                break;
-            }
-            
-            if(number % 2 == 0) number /=2;
-            else number = number * 3 + 1;
-            
-            answer++;
+        int cnt = 0;
+        long result = num;
+        while(result != 1){
+            result = (result % 2 == 0) ? result / 2 : result * 3 + 1;
+            cnt++;
+            if(cnt > 500) return -1;
         }
-        
-        return answer;
+        return cnt;
     }
 }
