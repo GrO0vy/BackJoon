ac16c289e9e3332817c97aeff2b60a82a3f16530
이민철 <89755903+GrO0vy@users.noreply.github.com>
2023-06-20 21:25:18 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 55.90 ms, Memory: 116 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Solution {
+    public int[] solution(int []arr) {
+        int[] answer = {};
+        
+        Stack<Integer> stack = new Stack<>();
+        
+        for(int num : arr){
+            if(stack.isEmpty() || num != stack.peek()) stack.push(num);
+        }
+
+        answer = new int[stack.size()];
+        
+        for(int i = answer.length - 1; i >= 0; i--){
+            answer[i] = stack.pop();
+        }
+        
+        return answer;
+    }
+}
