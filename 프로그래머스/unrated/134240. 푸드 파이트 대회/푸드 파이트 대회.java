4e8962ec858b9466bd99aec4b7aaab8862d9c54d
이민철 <89755903+GrO0vy@users.noreply.github.com>
2023-07-03 21:28:59 +0900

    [unrated] Title: 푸드 파이트 대회, Time: 9.52 ms, Memory: 81.6 MB -BaekjoonHub

--- 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.java
+++ 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -1,11 +1,13 @@
 class Solution {
     public String solution(int[] food) {
         String answer = "";
+        StringBuilder sb = new StringBuilder();
+        
         for(int i = 1; i < food.length; i++){
-            answer += String.valueOf(i).repeat(food[i] / 2);
+            sb.append(String.valueOf(i).repeat(food[i] / 2));
         }
-        StringBuilder sb = new StringBuilder(answer);
-        answer = answer + "0" + sb.reverse().toString();
+        
+        answer = sb.toString() + "0" + sb.reverse().toString();
         
         return answer;
     }
