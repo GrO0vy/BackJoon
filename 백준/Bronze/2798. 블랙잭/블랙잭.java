97f66e58daab4b85bb910f75e97e9eab87f5585e
이민철 <89755903+GrO0vy@users.noreply.github.com>
2023-07-05 09:27:15 +0900

    [Bronze II] Title: 블랙잭, Time: 128 ms, Memory: 14308 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.java
+++ 백준/Bronze/2798. 블랙잭/블랙잭.java
@@ -1,24 +1,35 @@
-import java.util.Scanner;
+import java.util.StringTokenizer;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
 
-public class Main {
-    public static void main(String[] args) {
-            Scanner sc = new Scanner(System.in);
-            int count = sc.nextInt();
-            int max = sc.nextInt();
-            sc.nextLine();
-            String[] numbers = sc.nextLine().split(" ");
-            int result = 0;
+public class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        // 카드 갯수, 목표 숫자
+        StringTokenizer infoToken = new StringTokenizer(reader.readLine());
+        int cardCount = Integer.parseInt(infoToken.nextToken());
+        int target = Integer.parseInt(infoToken.nextToken());
 
-            for(int i = 0; i < count; i++){
-                for(int j=i+1; j<count; j++){
-                    for(int k=j+1; k<count; k++){
-                        int temp = Integer.parseInt(numbers[i]) + Integer.parseInt(numbers[j]) + Integer.parseInt(numbers[k]);
-                        if(temp <= max && result < temp){
-                            result = temp;
-                        }
-                    }
+        // 카드 정보
+        StringTokenizer cardToken = new StringTokenizer(reader.readLine());
+        int[] cards = new int[cardCount];
+
+        for(int i = 0; i < cardCount; i++){
+            cards[i] = Integer.parseInt(cardToken.nextToken());
+        }
+
+        int max = 0;
+        // 3장의 카드를 뽑는다
+        for (int i = 0; i < cardCount - 2; i++) {
+            for (int j = i + 1; j < cardCount - 1; j++) {
+                for (int k = j + 1; k < cardCount; k++) {
+                    int sum = cards[i] + cards[j] + cards[k];
+                    if(sum <= target && sum > max) max = sum;
                 }
             }
-            System.out.println(result);
+        }
+        
+        System.out.println(max);
     }
 }
