b41cac2c2d576b92364edf7d1b2504f5171db86f
이민철 <89755903+GrO0vy@users.noreply.github.com>
2023-10-20 17:20:30 +0900

    [Bronze I] Title: 평균, Time: 128 ms, Memory: 14292 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -8,26 +8,19 @@ public class Main{
         int N = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
         
-        int[] scores = new int[N];
+        double sum = 0;
         int maxScore = 0;
         
         for(int i = 0; i < N; i++){
             int score = Integer.parseInt(st.nextToken());
-            scores[i] = score;
+            sum += score;
             
             if(score > maxScore) maxScore = score;
         }
         
-        double sum = 0;
-        double avg = 0;
-        
-        for(int i = 0; i < N; i++){
-            sum += scores[i] / (double)maxScore * 100;
-        }
-        
-        avg = sum / (double)N;
-        
-        System.out.println(avg);
+        double result = sum / maxScore * 100 / N;
+       
+        System.out.println(result);
         
         br.close();
     }
