faffce6c90a662f9ff0cf11e09cedb842a916501
이민철 <89755903+GrO0vy@users.noreply.github.com>
2023-10-20 17:12:50 +0900

    [Bronze I] Title: 평균, Time: 132 ms, Memory: 14332 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -1,30 +1,34 @@
-import java.util.Scanner;
-import java.util.ArrayList;
-import java.util.Collections;
+import java.io.*;
+import java.util.StringTokenizer;
 
 public class Main{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        ArrayList<Integer> scores = new ArrayList<Integer>();
-        ArrayList<Double> result = new ArrayList<Double>();
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
-        for(int i=0; i<N; i++){
-            int score = sc.nextInt();
-            scores.add(score);
-        }
-        int max = Collections.max(scores);
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
         
-        for(double score : scores) {
-        	double update = (double)score/(double)max*100;
-        	result.add(update);
+        int[] scores = new int[N];
+        int maxScore = 0;
+        
+        for(int i = 0; i < N; i++){
+            int score = Integer.parseInt(st.nextToken());
+            scores[i] = score;
+            
+            if(score > maxScore) maxScore = score;
         }
-        double total =0;
-        for(double score : result) {
-        	total += score;
+        
+        double sum = 0;
+        double avg = 0;
+        
+        for(int i = 0; i < N; i++){
+            sum += scores[i] / (double)maxScore * 100;
         }
-        double avg = total/N;
+        
+        avg = sum / (double)N;
+        
         System.out.println(avg);
-        sc.close();
+        
+        br.close();
     }
 }
