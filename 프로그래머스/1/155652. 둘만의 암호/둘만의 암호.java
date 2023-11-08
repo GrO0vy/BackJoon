09277548a9c88c25c2b544256133509c36f8d6a2
이민철 <89755903+GrO0vy@users.noreply.github.com>
2023-11-08 13:28:31 +0900

    [level 1] Title: 둘만의 암호, Time: 1.74 ms, Memory: 72.5 MB -BaekjoonHub

--- 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.java
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.java
@@ -1,21 +1,26 @@
+import java.util.List;
+import java.util.ArrayList;
+
 class Solution {
     public String solution(String s, String skip, int index) {
         String answer = "";
         
-        for(int i = 0; i < s.length(); i++){
-            char c = s.charAt(i);
-            int count = 0;
-            
-            while(count < index){
-                if(c < 'z') c++;
-                else c = 'a';
-                
-                if(skip.contains(c+"")) continue;
-                else count++;
-            }
-            
-            answer += c;
+        List<Character> alpha = new ArrayList<>();
+        for(char c = 'a'; c <= 'z'; c++){
+            alpha.add(c);
         }
+        
+        for(Character c : skip.toCharArray()){
+            alpha.remove(c);
+        }
+        
+        for(Character c: s.toCharArray()){
+            int idx = alpha.indexOf(c);
+            
+            int change = (idx + index) % alpha.size();
+            answer += alpha.get(change);
+        }
+        
         return answer;
     }
 }
