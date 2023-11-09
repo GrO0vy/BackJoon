b40c0e697a4856dcab63d8c7ae32e3128c8c8913
이민철 <89755903+GrO0vy@users.noreply.github.com>
2023-11-09 17:43:20 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 1660 ms, Memory: 336784 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
@@ -6,7 +6,7 @@ public class Main{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         
-        int[] arr = new int[10000001];
+        int[] arr = new int[10001];
         
         for(int i = 0; i < N; i++){
             arr[Integer.parseInt(br.readLine())] += 1;      
