9caade46d27e5236c2a23b3d63afce2cc65f7b01
이민철 <89755903+GrO0vy@users.noreply.github.com>
2023-11-17 12:19:05 +0900

    [Silver III] Title: 소수 구하기, Time: 308 ms, Memory: 26628 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int M = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+
+        int[] numbers = new int[N + 1];
+        for(int i = 2; i <= N; i++){
+            numbers[i] = i;
+        }
+
+        for(int i = 2; i <= Math.sqrt(N); i++){
+            for(int j = 2 * i; j <= N; j += i){
+                if(numbers[j] == 0) continue;
+                numbers[j] = 0;
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for(int i = M; i <= N; i++) if(numbers[i] != 0) sb.append(i + "\n");
+        System.out.println(sb);
+    }
+}
