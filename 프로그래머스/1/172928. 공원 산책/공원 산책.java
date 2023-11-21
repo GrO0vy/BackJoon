cae464c4d38ab58873a02dd22d9c54492ce6de7c
이민철 <89755903+GrO0vy@users.noreply.github.com>
2023-11-21 17:06:44 +0900

    [level 1] Title: 공원 산책, Time: 0.20 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/1/172928. 공원 산책/공원 산책.java
+++ 프로그래머스/1/172928. 공원 산책/공원 산책.java
@@ -1,91 +1,83 @@
-import java.util.*;
+import java.util.StringTokenizer;
 
 class Solution {
     public int[] solution(String[] park, String[] routes) {
+        int[] answer = {};
         
-        // 이차원 배열을 새로 만들면서, 로봇의 시작 위치를 찾는다.
-        int robotRow = -1;
-        int robotColumn = -1;
-        
-        int rowNum = park.length;
-        int columnNum = park[0].length();
-            
-        char [][] parkArray = new char[rowNum][columnNum];
-        
-        for (int i=0 ; i< rowNum; i++) { //50
-            String rowData = park[i];
-            for (int j=0 ; j<columnNum ;j++) { //50
-                
-                char locationData = rowData.charAt(j);
-                parkArray[i][j] = locationData;
-                
-                if (locationData == 'S') {
-                    robotRow = i;
-                    robotColumn = j;
-                }
+        for(int i = 0; i < park.length; i++){
+            for(int j = 0; j < park[i].length(); j++){
+                if(park[i].charAt(j) == 'S') answer = new int[]{i, j};
             }
         }
         
-        
-        // 좌표 값 저장        
-        Map<Character, List<Integer>> dirMove = new HashMap<Character, List<Integer>>();
-        dirMove.put('E', List.of(0,1));
-        dirMove.put('W', List.of(0,-1));
-        dirMove.put('N', List.of(-1,0));
-        dirMove.put('S', List.of(1,0));
-        
-
-        // routes 만큼 반복한다. 
-        for (String route : routes) {
+        for(String route: routes){
+            StringTokenizer st = new StringTokenizer(route);
             
-            char direction = route.charAt(0);
-            Integer move = route.charAt(2) - '0';
+            String direction = st.nextToken();
+            int distance = Integer.parseInt(st.nextToken());
             
-            
-            // 이동할 위치 계산 
-            // 현재 위치를 먼저 저장한다. (현재 명령이 불가능하다면 롤백을 해주기 위함)
-            int tmpRow = robotRow;
-            int tmpColumn = robotColumn;
-            
-            Boolean possible = true;
-            
-            // 해당 방향으로 몇칸을 이동해야해서, 한칸씩 이동하며 가능성을 확인한다. 
-            for (int i=0; i<move; i++) {
+            if(direction.equals("E")){
+                if(answer[1] + distance < park[0].length()){
+                    boolean isMoveable = true;
+                    for(int i = answer[1] + 1; i <= answer[1] + distance ; i++){
+                        if(park[answer[0]].charAt(i) == 'X') {
+                            isMoveable = false;
+                            break;
+                        }
+                    }
+                    if(!isMoveable) continue;
+                }
+                else continue;
                 
-                Integer moveRow = robotRow + dirMove.get(direction).get(0);
-                Integer moveColumn = robotColumn + dirMove.get(direction).get(1);
-            
-                // 공원 바깥이면 실패
-                if ( 0 > moveRow || moveRow >= rowNum || 0 > moveColumn || moveColumn >= columnNum) {
-                    possible = false;
-                    break;
-                }
-
-                // 장애물을 만나면 실패
-                if (parkArray[moveRow][moveColumn] == 'X') {
-                    possible = false;
-                    break;
-                }
-            
-                //실제 이동
-                parkArray[moveRow][moveColumn] = 'S';
-                parkArray[robotRow][robotColumn] = 'O';
-            
-                robotRow = moveRow;
-                robotColumn = moveColumn;
+                answer[1] += distance;
             }
-            
-            //불가능한 경우가 존재하면 롤백
-            if (possible == false) {
-                robotRow = tmpRow ;
-                robotColumn = tmpColumn;
+            else if(direction.equals("W")){
+                if(answer[1] - distance >= 0){
+                    boolean isMoveable = true;
+                    for(int i = answer[1] - 1; i >= answer[1] - distance ; i--){
+                        if(park[answer[0]].charAt(i) == 'X') {
+                            isMoveable = false;
+                            break;
+                        }
+                    }
+                    if(!isMoveable) continue;
+                }
+                else continue;
+                
+                answer[1] -= distance;
+            }
+            else if(direction.equals("S")){
+                if(answer[0] + distance < park.length){
+                    boolean isMoveable = true;
+                    for(int i = answer[0] + 1; i <= answer[0] + distance ; i++){
+                        if(park[i].charAt(answer[1]) == 'X'){
+                            isMoveable = false;
+                            break;
+                        }
+                    }
+                    if(!isMoveable) continue;
+                }
+                else continue;
+                
+                answer[0] += distance;
+            }
+            else{
+                if(answer[0] - distance >= 0){
+                    boolean isMoveable = true;
+                    for(int i = answer[0] - 1; i >= answer[0] - distance ; i--){
+                        if(park[i].charAt(answer[1]) == 'X') {
+                            isMoveable = false;
+                            break;
+                        }
+                    }
+                    if(!isMoveable) continue;
+                }
+                else continue;
+                
+                answer[0] -= distance;
             }
         }
         
-        // 마지막에 로봇의 위치를 배열에 저장해서 return하면 끝
-        int[] answer = new int[2];
-        answer[0] = robotRow;
-        answer[1] = robotColumn;
         return answer;
     }
 }
