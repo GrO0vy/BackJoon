d649b2ed315a9e01690255b1c47ddbf409d3f40f
이민철 <89755903+GrO0vy@users.noreply.github.com>
2023-12-01 19:10:18 +0900

    [level 2] Title: 올바른 괄호, Time: 19.81 ms, Memory: 52.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,26 @@
+import java.util.Stack;
+
+class Solution {
+    boolean solution(String s) {
+        boolean answer = true;
+
+        Stack<Character> stack = new Stack<>();
+        
+        for(int i = 0; i < s.length(); i++){
+            char token = s.charAt(i);
+            
+            if(token == '(') stack.push(token);
+            else{
+                if(stack.isEmpty()){
+                    answer = false;
+                    break;
+                }
+                else stack.pop();
+            }
+        }
+        
+        if(!stack.isEmpty()) answer = false;
+
+        return answer;
+    }
+}
