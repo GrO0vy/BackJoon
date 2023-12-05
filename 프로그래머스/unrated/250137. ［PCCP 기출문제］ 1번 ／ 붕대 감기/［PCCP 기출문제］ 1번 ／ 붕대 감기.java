9269876bf297ca88bc1ab5c83fbb5825268ddf12
이민철 <89755903+GrO0vy@users.noreply.github.com>
2023-12-05 17:25:35 +0900

    [level unrated] Title: [PCCP 기출문제] 1번 / 붕대 감기, Time: 0.51 ms, Memory: 78.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.java
@@ -0,0 +1,42 @@
+import java.util.HashMap;
+
+class Solution {
+    public int solution(int[] bandage, int health, int[][] attacks) {
+        int answer = 0;
+        
+        int currentHealth = health;
+        int maxCombo = bandage[0];
+        int heal = bandage[1];
+        int bonus = bandage[2];
+        int combo = 1;
+        int lastTime = attacks[attacks.length - 1][0];
+        
+        
+        HashMap<Integer, Integer> attack = new HashMap<>();
+        for(int[] att: attacks){
+            attack.put(att[0], att[1]);    
+        }
+            
+        for(int i = 1; i <= lastTime; i++){
+            if(!attack.containsKey(i)){
+                if(combo == maxCombo) {
+                    currentHealth += bonus + heal;
+                    combo = 1;
+                }
+                else {
+                    currentHealth += heal;
+                    combo++;
+                }
+                
+                if(currentHealth > health) currentHealth = health;
+            }
+            else{
+                currentHealth -= attack.get(i);
+                if(currentHealth <= 0) return -1;
+                combo = 1;
+            }
+        }
+        
+        return currentHealth < 0 ? -1 : currentHealth;
+    }
+}
