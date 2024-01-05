87712c47b853aa683484c6b70350ff817af3946f
이민철 <89755903+GrO0vy@users.noreply.github.com>
2024-01-05 21:59:27 +0900

    [level 2] Title: 귤 고르기, Time: 88.77 ms, Memory: 120 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
@@ -1,33 +1,26 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 class Solution {
     public int solution(int k, int[] tangerine) {
         int answer = 0;
-        Map<Integer, Integer> map = new HashMap<>();
-
-        // Map에 귤의 크기별 개수 담기
-        for (int i : tangerine) {
-            map.put(i, map.getOrDefault(i, 0) + 1);
+        
+        Arrays.sort(tangerine);
+        int max = tangerine[tangerine.length - 1];
+        
+        int[] arr = new int[max + 1];
+        for(int i = 0; i < tangerine.length; i++){
+            arr[tangerine[i]]++;
         }
-
-        // 귤의 크기 리스트 생성
-        List<Integer> keyList = new ArrayList<>(map.keySet());
-        // 귤의 개수별 내림차순 정렬
-        keyList.sort(((o1, o2) -> map.get(o2) - map.get(o1)));
-
-        // k의 개수 만큼 상자에 담기(종류의 최소값 추출)
-        for (Integer i : keyList) {
-            if (k <= 0) {
-                break;
-            }
-
+        
+        Arrays.sort(arr);
+        
+        int idx = max;
+        while(k > 0 && idx > 0){
+            k -= arr[idx];
             answer++;
-            k -= map.get(i);
+            idx--;
         }
-
+        
         return answer;
     }
 }
