db961980361317486c3530ddee5fa5deffd6ad00
이민철 <89755903+GrO0vy@users.noreply.github.com>
2024-01-05 21:33:38 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.21 ms, Memory: 72.1 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -1,14 +1,15 @@
 class Solution {
-        public long solution(int n) {
-        long arr []= new long[2001];
-
-	    arr[1]=1;
-	    arr[2]=2;
-	 
-	    for(int i=3; i<2001; i++) {
-		    arr[i] = (arr[i-1]+arr[i-2])%1234567;			 
-	    }		 
-	        return arr[n];
+    public long solution(int n) {
+        long answer = 0;
+        long[] arr = new long[n + 1];
+        arr[0] = 1;
+        arr[1] = 2;
         
+        for(int i = 2; i < n; i++){
+            arr[i] = (arr[i - 1] + arr[i - 2]) % 1234567;
+        }
+        
+        answer = arr[n - 1];
+        return answer;
     }
 }
