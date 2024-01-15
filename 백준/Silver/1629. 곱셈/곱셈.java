80cec3e72a33b6c3c3a639348f12172f0c7c2496
이민철 <89755903+GrO0vy@users.noreply.github.com>
2024-01-15 16:05:29 +0900

    [Silver I] Title: 곱셈, Time: 120 ms, Memory: 14232 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.java
+++ 백준/Silver/1629. 곱셈/곱셈.java
@@ -1,17 +1,27 @@
 import java.io.*;
 import java.util.*;
-import java.math.BigInteger;
 
 public class Main{
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        
-        BigInteger A = new BigInteger(st.nextToken());
-        BigInteger B = new BigInteger(st.nextToken());
-        BigInteger C = new BigInteger(st.nextToken());
-            
-        System.out.println(A.modPow(B,C));
+
+        long A = Long.parseLong(st.nextToken());
+        long B = Long.parseLong(st.nextToken());
+        long C = Long.parseLong(st.nextToken());
+
+        long answer = divide(A, B, C);
+
+        System.out.println(answer);
     }
 
+    static long divide(long A, long B, long C){
+        if(B == 1) return A % C;
+        else if(B % 2 == 0) return twice(divide(A, B/2, C)) % C;
+        else return A * divide(A, B - 1, C) % C;
+    }
+
+    private static long twice(long num){
+        return num * num;
+    }
 }
