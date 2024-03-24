09766a35bae00a9537e300fb952e3012ee85f48d
이민철 <89755903+GrO0vy@users.noreply.github.com>
2024-03-24 11:03:10 +0900

    [Gold V] Title: 수 고르기, Time: 372 ms, Memory: 28032 KB -BaekjoonHub

---
+++ 백준/Gold/2230. 수 고르기/수 고르기.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[] A = new int[N];
+
+        for(int i = 0; i < N; i++) A[i] = Integer.parseInt(br.readLine());
+
+        Arrays.sort(A);
+
+        int left = 0;
+        int right = 1;
+        int min = Integer.MAX_VALUE;
+
+        while(right < A.length){
+            if(left == right){
+                right++;
+                continue;
+            }
+
+            int gap = Math.abs(A[left] - A[right]);
+            if(gap < M) right++;
+            else{
+                min = Math.min(min, gap);
+                left++;
+            }
+        }
+
+        System.out.println(min);
+    }
+}
