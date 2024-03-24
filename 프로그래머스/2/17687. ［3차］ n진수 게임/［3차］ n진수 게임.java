cc99b03a68a17b336ad04ebae39f42b88d3b36ca
이민철 <89755903+GrO0vy@users.noreply.github.com>
2024-03-24 15:14:39 +0900

    [level 2] Title: [3차] n진수 게임, Time: 33.85 ms, Memory: 99.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
@@ -0,0 +1,32 @@
+class Solution {
+    public String solution(int n, int t, int m, int p) {
+        String answer = "";
+        
+        String base = "";
+        int num = 0;
+        while(base.length() < m * t){
+            base += tenToN(num, n);
+            num++;
+        }
+        
+        System.out.println(base);
+        for(int i = 0; answer.length() < t; i += m){
+            answer += base.charAt(i + p - 1) + "";
+        }
+        
+        return answer;
+    }
+    
+    public String tenToN(int decimal, int n){
+        String base = "";
+        while(decimal >= n){
+            int next = decimal % n;
+            
+            if(next >= 10) base = (char)(next + 55) + base;
+            else base = ( decimal % n ) + base;
+            decimal /= n;
+        }
+        
+        return decimal >= 10 ? (char)(decimal + 55) + base : decimal + base;
+    }
+}
