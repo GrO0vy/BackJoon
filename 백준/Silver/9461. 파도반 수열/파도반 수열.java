adf60304923a79ce3b72b6aad6c4154297f37367
이민철 <89755903+GrO0vy@users.noreply.github.com>
2024-03-25 11:08:08 +0900

    [Silver III] Title: 파도반 수열, Time: 244 ms, Memory: 17892 KB -BaekjoonHub

---
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+
+        for(int i = 0; i < T; i++){
+            int N = sc.nextInt();
+            long[] arr = new long[N + 3];
+            arr[1] = 1; arr[2] = 1; arr[3] = 1;
+
+            for(int j = 4; j <= N; j++){
+                arr[j] = arr[j - 3] + arr[j - 2];
+            }
+
+            System.out.println(arr[N]);
+        }
+    }
+}
