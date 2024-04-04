90e64116748ea1b1b6e498c16ffefde3febbde38
이민철 <89755903+GrO0vy@users.noreply.github.com>
2024-04-04 15:37:41 +0900

    [level 2] Title: 스킬트리, Time: 8.94 ms, Memory: 71.2 MB -BaekjoonHub

--- 프로그래머스/2/49993. 스킬트리/스킬트리.java
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.java
@@ -3,20 +3,9 @@ class Solution {
         int answer = 0;
         
         for(String skill_tree: skill_trees){
-            String notLearn = skill;
-            boolean isPossible = true;
-            for(int j = 0; j < skill_tree.length(); j++){
-                if(notLearn.contains(skill_tree.charAt(j) + "")){
-                    if(skill_tree.length() > 0 && 
-                       skill_tree.charAt(j) == notLearn.charAt(0))
-                        notLearn = notLearn.substring(1);
-                    else isPossible = false;
-                }
-            }
-            
-            if(isPossible) answer++;
+            skill_tree = skill_tree.replaceAll("[^"+ skill +"]", "");
+            if(skill.indexOf(skill_tree) == 0) answer++;
         }
-        
         return answer;
     }
 }
