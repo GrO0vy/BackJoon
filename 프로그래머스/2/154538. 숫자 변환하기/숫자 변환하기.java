49613b8d6db284701871d89858577ed45985ad59
이민철 <89755903+GrO0vy@users.noreply.github.com>
2024-04-06 10:44:11 +0900

    [level 2] Title: 숫자 변환하기, Time: 11.40 ms, Memory: 81.8 MB -BaekjoonHub

--- 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.java
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.java
@@ -3,23 +3,22 @@ import java.util.*;
 class Solution {
 
     public int solution(int x, int y, int n) {
+        int answer = -1;
         int[] dp = new int[y + 1];
-        Arrays.fill(dp, y + 1);
-        dp[x] = 0;
-        for (int i = x; i <= y; i++) {
-            if (dp[i] == y + 1) {
-                continue;
-            }
-            if (i + n <= y) {
-                dp[i + n] = Math.min(dp[i + n], dp[i] + 1);
-            }
-            if (i * 2 <= y) {
-                dp[i * 2] = Math.min(dp[i * 2], dp[i] + 1);
-            }
-            if (i * 3 <= y) {
-                dp[i * 3] = Math.min(dp[i * 3], dp[i] + 1);
-            }
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[y] = 0;
+        
+        for(int i = y; i >= x; i--){
+            if(dp[i] == Integer.MAX_VALUE) continue;
+            if(i % 3 == 0 && i / 3 >= x) 
+                dp[i / 3] = Math.min(dp[i / 3], dp[i] + 1);
+            if(i % 2 == 0 && i / 2 >= x) 
+                dp[i / 2] = Math.min(dp[i / 2], dp[i] + 1);
+            if(i - n >= x) dp[i - n] = Math.min(dp[i - n], dp[i] + 1);
         }
-        return dp[y] == y + 1 ? -1 : dp[y];
+        
+        answer = dp[x] == Integer.MAX_VALUE ? answer : dp[x];
+        
+        return answer;
     }
 }
