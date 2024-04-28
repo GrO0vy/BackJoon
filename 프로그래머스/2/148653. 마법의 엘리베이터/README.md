96a45bfb528b86e6f8db0f65693ef703b57f37fc
이민철 <89755903+GrO0vy@users.noreply.github.com>
2024-04-29 00:37:53 +0900

    [level 2] Title: 마법의 엘리베이터, Time: 0.01 ms, Memory: 72.9 MB -BaekjoonHub

--- 프로그래머스/2/148653. 마법의 엘리베이터/README.md
+++ 프로그래머스/2/148653. 마법의 엘리베이터/README.md
@@ -1,10 +1,10 @@
 # [level 2] 마법의 엘리베이터 - 148653 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/148653) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/148653#) 
 
 ### 성능 요약
 
-메모리: 79.3 MB, 시간: 0.02 ms
+메모리: 72.9 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 28일 23:57:36
+2024년 04월 29일 00:37:45
 
 ### 문제 설명
 
--- 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.java
+++ 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.java
@@ -1,30 +1,23 @@
 class Solution {
     public int solution(int storey) {
-    	String tmp = Integer.toString(storey);
-    	int[] arr = new int[tmp.length()];
-    	for(int i=0; i<tmp.length(); i++) {
-    		arr[i] = tmp.charAt(i) - '0';
-    	}
-    	
-    	int answer = 0;
-    	
-    	for(int i=arr.length-1; i>=0; i--) {
-    		if(arr[i] > 5) {
-    			answer += 10-arr[i];
-    			
-    			if(i==0) answer++;
-    			else arr[i-1]++;
-    		}
-    		else if(arr[i]==5 && i>0 && arr[i-1]>=5) {
-    			arr[i-1]++;
-    			answer += 5;
-    		}
-    		else {
-    			answer += arr[i];
-    		}
-
-    	}
-    	
-    	return answer;
+        int answer = 0;
+        
+        while(storey > 0){
+            int digit = storey % 10;
+            
+            storey /= 10;
+            
+            if(digit == 5){
+                answer += digit;
+                if(storey / 10 > 0 && storey % 10 >= 5) storey++;
+            }
+            else if(digit > 5) {
+                answer += 10 - digit;
+                storey++;
+            }
+            else answer += digit;
+        }
+        
+        return answer;
     }
 }
