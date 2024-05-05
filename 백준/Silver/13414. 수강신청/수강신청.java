013e3ef992026817ec067396eb2a4eff00e39c2b
이민철 <89755903+GrO0vy@users.noreply.github.com>
2024-05-05 22:10:42 +0900

    [Silver III] Title: 수강신청, Time: 1428 ms, Memory: 68948 KB -BaekjoonHub

--- 백준/Silver/13414. 수강신청/수강신청.java
+++ 백준/Silver/13414. 수강신청/수강신청.java
@@ -1,23 +1,26 @@
 import java.util.*;
-public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int k = sc.nextInt();
-        int l = sc.nextInt();
-        int count = 0;
+import java.io.*;
 
+public class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int K = Integer.parseInt(st.nextToken());
+        int L = Integer.parseInt(st.nextToken());
+        
         LinkedHashSet<String> set = new LinkedHashSet<>();
-
-        for(int i=0; i<l; i++){
-            String str = sc.next();
-
-            if(set.contains(str)) set.remove(str);
-            set.add(str);
+        for(int i = 0; i < L; i++){
+            String student = br.readLine();
+            if(set.contains(student)) set.remove(student);
+            set.add(student);
         }
-        for(String x : set){
-            count++;
-            System.out.println(x);
-            if(count == k) break;
+        
+        int cnt = 0;
+        for(String student: set){
+            if(cnt == K) break;
+            System.out.println(student);
+            cnt++;
         }
     }
 }
