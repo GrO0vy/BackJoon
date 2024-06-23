ec5ac2c4d1bcde71ba0502f4f59a06b94d2d5baf
이민철 <89755903+GrO0vy@users.noreply.github.com>
2024-06-23 15:41:39 +0900

    [Silver II] Title: 싸이버개강총회, Time: 652 ms, Memory: 70260 KB -BaekjoonHub

--- 백준/Silver/19583. 싸이버개강총회/싸이버개강총회.java
+++ 백준/Silver/19583. 싸이버개강총회/싸이버개강총회.java
@@ -1,42 +1,40 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
-public class Main {
-    public static void main(String[] args) throws IOException {
+public class Main{
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        String S = st.nextToken();
-        String E = st.nextToken();
-        String Q = st.nextToken();
 
-        Set<String> before = new HashSet<>();
-        Set<String> after = new HashSet<>();
-        Set<String> nameSet = new HashSet<>();
-        String str = null;
-        
-        while((str = br.readLine()) != null) {
-            String[] arr = str.split(" ");
-            String time = arr[0];
-            String name = arr[1];
+        int start = calTime(st.nextToken());
+        int end = calTime(st.nextToken());
+        int sEnd = calTime(st.nextToken());
+        int answer = 0;
 
-            nameSet.add(name);
-            if(S.compareTo(time) >= 0) {
-                before.add(name);
-            }else if(E.compareTo(time) <= 0 && Q.compareTo(time) >= 0) {
-                after.add(name);
+        HashSet<String> set = new HashSet<>();
+
+        while(true){
+            String input = br.readLine();
+            if(input == null || "".equals(input)) break;
+
+            st = new StringTokenizer(input);
+            int curTime = calTime(st.nextToken());
+            String stu = st.nextToken();
+
+            if(curTime <= start) set.add(stu);
+
+            if(set.contains(stu) && curTime >= end && curTime <= sEnd){
+                answer++;
+                set.remove(stu);
             }
         }
 
-        int ans = 0;
-        for(String name : nameSet) {
-            if(before.contains(name) && after.contains(name)) {
-                ans += 1;
-            }
-        }
-        System.out.println(ans);
+        System.out.println(answer);
+    }
+
+    static int calTime(String str){
+        String[] time = str.split(":");
+
+        return 60 * Integer.parseInt(time[0]) + Integer.parseInt(time[1]);
     }
 }
