2c2530dbe99f699093b64accb122108f860e4ea6
이민철 <89755903+GrO0vy@users.noreply.github.com>
2024-08-03 13:10:33 +0900

    [Gold V] Title: 용액 합성하기, Time: 296 ms, Memory: 25624 KB -BaekjoonHub

--- 백준/Gold/14921. 용액 합성하기/용액 합성하기.java
+++ 백준/Gold/14921. 용액 합성하기/용액 합성하기.java
@@ -1,33 +1,36 @@
+import java.util.*;
 import java.io.*;
-import java.util.Arrays;
-import java.util.StringTokenizer;
- 
-public class Main {
- 
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		int[] arr = new int[N];
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < N; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-		}
-		int left = 0;
-		int right = N-1;
-		int min = Integer.MAX_VALUE;
-		while(true) {
-			if(left==right) break;
-			int sum = arr[left] + arr[right];
-			if(Math.abs(sum) <= Math.abs(min)) {
-				min = sum;
-			}
-			if(sum < 0) {
-				left++;
-			} else {
-				right--;
-			}
-			
-		}
-		System.out.println(min);
-	}
+
+public class Main{
+    static int[] arr;
+
+    public static void main(String[] args)  throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        arr = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i = 0; i< N; i++) arr[i] = Integer.parseInt(st.nextToken());
+
+        Arrays.sort(arr);
+
+        int left = 0;
+        int right = N - 1;
+        int result = Integer.MAX_VALUE;
+
+        while(left < right){
+            int sum = arr[left] + arr[right];
+            if(Math.abs(sum) < Math.abs(result)) result = sum;
+
+            if(sum < 0) left++;
+            else if(sum > 0) right--;
+            else {
+                result = sum;
+                break;
+            }
+        }
+
+        System.out.println(result);
+    }
 }
