54d68709b78fa8cfd2d9db35322c7d8671894c40
이민철 <89755903+GrO0vy@users.noreply.github.com>
2024-09-23 14:45:27 +0900

    [Silver V] Title: 거스름돈, Time: 168 ms, Memory: 17712 KB -BaekjoonHub

--- 백준/Silver/14916. 거스름돈/거스름돈.java
+++ 백준/Silver/14916. 거스름돈/거스름돈.java
@@ -7,19 +7,16 @@ public class Main{
         int n = sc.nextInt();
         int count = 0;
         
-        if(n % 5 == 0) count = n / 5;
-        else{
-            while(n > 0){
+        while(n > 0){
+            if(n % 5 == 0){
+                count += n / 5;
+                n %= 5;
+                break;
+            }
                 n -= 2;
-                count++;
-                if(n % 5 == 0){
-                    count += n / 5;
-                    n %= 5;
-                    break;
-                }
-            }    
+                count++;   
         }
-        
+      
         if(n < 0) System.out.println(-1);
         else System.out.println(count);
     }
