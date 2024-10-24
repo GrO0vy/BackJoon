be8ba383d3db6b6f0bfa2dd494c8c6d7146fcb17
이민철 <lmc2819@naver.com>
2024-10-24 14:51:10 +0900

    [Silver III] Title: N과 M (6), Time: 120 ms, Memory: 15852 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    static int N;
+    static int M;
+    static int[] numbers;
+    static int[] arr;
+    static StringBuilder sb;
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        numbers = new int[N];
+        arr = new int[M];
+
+        sb = new StringBuilder();
+        st = new StringTokenizer(br.readLine());
+
+        for(int i = 0; i < N; i++) numbers[i] = Integer.parseInt(st.nextToken());
+
+        Arrays.sort(numbers);
+
+        searchSequence(0, 0);
+
+        System.out.println(sb);
+    }
+
+    static void searchSequence(int depth, int start){
+        if(M == depth){
+            for(int i = 0; i < M; i++){
+                sb.append(arr[i] + " ");
+            }
+            sb.append("\n");
+
+            return;
+        }
+
+        for(int i = start; i < N; i++){
+            arr[depth] = numbers[i];
+            searchSequence(depth + 1, i + 1);
+        }
+    }
+}
