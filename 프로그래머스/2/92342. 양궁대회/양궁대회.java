9c67fe79096ecc873529cfa75ba8a9674760d51f
이민철 <lmc2819@naver.com>
2025-06-28 22:11:55 +0900

    [level 2] Title: 양궁대회, Time: 0.67 ms, Memory: 87 MB -BaekjoonHub

--- 프로그래머스/2/92342. 양궁대회/양궁대회.java
+++ 프로그래머스/2/92342. 양궁대회/양궁대회.java
@@ -1,47 +1,59 @@
-public class Solution {
-	static private int[] res = new int[11];//점수차가 최대일때 라이언이 쏜 화살배열
-	static private int[] lion= {-1};//정답배열
-	static private int max = Integer.MIN_VALUE;//최대값
-    public static int[] solution(int n, int[] info) {
-        back(0,n,info);//라이언이 쏜 화살에 대한 조합
+import java.util.*;
+
+class Solution {
+    int max = 0;
+    int[] answer;
+    int[] apeach;
+    
+    public int[] solution(int n, int[] info) {
+        answer = new int[11];
+        apeach = info;
         
-        if(max==-1) {//라이언이 어피치를 못 이길떄
-        	lion = new int[1];
-        	lion[0]=-1;
+        dfs(0, new int[11], n);
+        
+        return max > 0 ? answer : new int[]{-1};
+    }
+    
+    void dfs(int index, int[] lion, int arrows){
+        if(index == 11){
+            lion[10] = arrows;
+            
+            int diff = calDiff(lion);
+            
+            if(diff > max){
+                max = diff;
+                answer = Arrays.copyOf(lion, lion.length);
+            }
+            else if(diff == max){
+                for(int i = 10; i >= 0; i--){
+                    if(lion[i] == answer[i]) continue;
+                    
+                    if(lion[i] > answer[i]) answer = Arrays.copyOf(lion, lion.length);
+                    break;
+                }
+            }
         }
-        return lion;
+        else{
+            if(arrows > apeach[index]){
+                lion[index] = apeach[index] + 1;
+                dfs(index + 1, lion, arrows - lion[index]);
+                lion[index] = 0;    
+            }
+            
+            dfs(index + 1, lion, arrows);
+        }
     }
     
-    public static void back(int depth, int n, int[] info) {
-    	//화살 다 꽂았을때(종료조건)
-    	if(depth==n) {
-    		int diff = score(info);//점수차 구하기
-    		if(max<=diff) {//점수차 최대값 갱신
-    			max = diff;
-    			lion = res.clone();
-    		}
-    		return;
-    	}
-    	
-    	//res[i]<=info[i] -> i과녁에 라이언이 화살을 더 많이 맞추면 반복문 종료한다.
-    	for(int i=0; i<info.length && res[i]<=info[i]; i++) {
-    		res[i] += 1;
-    		back(depth+1, n, info);
-    		res[i] -= 1;
-    	}
-    }
-    
-    //점수차 구하기
-    public static int score(int[] info) {
-    	int apeach=0, lion=0;
-    	for(int i=0; i<res.length; i++) {
-    		if(info[i]==0 && res[i]==0) continue;//i원소에 둘다 0개 맞췄을땐 무시.
-    		if(info[i]>=res[i]) apeach += (10-i);
-    		else lion += (10-i);
-    	}
-    	
-    	int diff = lion - apeach;
-    	if(diff<=0) return -1;
-    	return diff;
+    int calDiff(int[] lion){
+        int diff = 0;
+        
+        for(int i = 0; i <= 10; i++){
+            if(lion[i] == 0 && apeach[i] == 0) continue;
+            
+            if(lion[i] > apeach[i]) diff += 10 - i;
+            else diff -= 10 - i;
+        }
+        
+        return diff;
     }
 }
