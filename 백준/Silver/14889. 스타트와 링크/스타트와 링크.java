18a3975d85b222b46e504158a2c6b19899825788
이민철 <lmc2819@naver.com>
2025-07-13 18:37:03 +0900

    [Silver I] Title: 스타트와 링크, Time: 436 ms, Memory: 17004 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
@@ -14,47 +14,40 @@ public class Main{
 
         min = Integer.MAX_VALUE;
 
-        int total = 0;
         for(int i = 0; i < N; i++){
             StringTokenizer st = new StringTokenizer(br.readLine());
             for(int j = 0; j < N; j++){
                 scores[i][j] = Integer.parseInt(st.nextToken());
-                total += scores[i][j];
             }
         }
 
         for(int i = 0; i < N; i++){
-            dfs(i, new ArrayList<>(), 0, total, 0);
+            dfs(i, new boolean[N],  0);
         }
 
         System.out.println(min);
     }
 
-    static void dfs(int start, List<Integer> players, int team, int opponent, int count){
+    static void dfs(int start, boolean[] check, int count){
         if(count == N / 2){
+            int team = 0;
+            int opponent = 0;
+
+            for(int i = 0; i < N; i++){
+                for(int j = 0; j < N; j++){
+                    if(check[i] && check[j]) team += scores[i][j];
+                    if(!check[i] && !check[j]) opponent += scores[i][j];
+                }
+            }
+
             min = Math.min(min, Math.abs(team - opponent));
             return;
         }
 
         for(int i = start; i < N; i++){
-            players.add(i);
-            int teamScore = team;
-            int opponentScore = opponent;
-
-            for(int coworker: players){
-                teamScore += scores[i][coworker] + scores[coworker][i];
-            }
-
-            for(int j = 0; j < N; j++) {
-                if(!players.contains((Integer) j)){
-                    opponentScore -= scores[j][i];
-                    opponentScore -= scores[i][j];
-                }
-            }
-
-            dfs(i + 1, players, teamScore, opponentScore, count + 1);
-
-            players.remove(players.size() - 1);
+            check[i] = true;
+            dfs(i + 1, check,count + 1);
+            check[i] = false;
         }
     }
 }
