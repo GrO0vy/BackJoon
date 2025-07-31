48419e53fe300c7d78fc968c80e0cdcaac89bf4f
이민철 <lmc2819@naver.com>
2025-07-31 16:38:20 +0900

    [Gold II] Title: 보석 도둑, Time: 1204 ms, Memory: 121600 KB -BaekjoonHub

--- 백준/Gold/1202. 보석 도둑/보석 도둑.java
+++ 백준/Gold/1202. 보석 도둑/보석 도둑.java
@@ -1,34 +1,49 @@
 import java.util.*;
 import java.io.*;
 
-public class Main{
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        int N = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken());
-
-        PriorityQueue<int[]> jewels = new PriorityQueue<>((o1, o2) -> o1[0] - o2[0]); // 0번째는 무게, 1번째는 가격
-        for(int i = 0; i < N; i++){
-            st = new StringTokenizer(br.readLine());
-            jewels.offer(new int[]{Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())});
-        }
-
-        int[] bags = new int[K];
-        for(int i = 0; i < K; i++) bags[i] = Integer.parseInt(br.readLine());
-
-        Arrays.sort(bags);
-
-        PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> o2[1] - o1[1]);
-        long result = 0;
-
-        for(int bag: bags){
-            while(!jewels.isEmpty() && bag >= jewels.peek()[0]) pq.offer(jewels.poll());
-
-            if(!pq.isEmpty()) result += pq.poll()[1];
-        }
-
-        System.out.println(result);
-    }
+class Main {
+	static class Jewel {
+		int M, V;
+		public Jewel(int M, int V) {
+			this.M = M;
+			this.V = V;
+		}
+		
+		public String toString() {
+			return "M : " + M + " | V : " + V;
+		}
+	}
+	
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+		PriorityQueue<Jewel> jewelPQ = new PriorityQueue<>((x, y) -> x.M - y.M);
+		PriorityQueue<Integer> bagPQ = new PriorityQueue<>((x, y) -> Integer.compare(x, y));
+		long answer = 0;
+		
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			jewelPQ.add(new Jewel(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
+		}
+		for (int i = 0; i < K; i++) {
+			bagPQ.add(Integer.parseInt(br.readLine()));
+		}
+		
+		PriorityQueue<Integer> maxJewelValuePQ = new PriorityQueue<>((x, y) -> y - x);
+		while(!bagPQ.isEmpty()) {
+			int limit = bagPQ.poll();
+			
+			while (!jewelPQ.isEmpty() && jewelPQ.peek().M <= limit) {
+				maxJewelValuePQ.add(jewelPQ.poll().V);
+			}
+			
+			if(!maxJewelValuePQ.isEmpty()) {
+				answer += maxJewelValuePQ.poll();
+			}
+		}
+		
+		System.out.println(answer);
+	}
 }
