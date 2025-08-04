ae03b3d7c0fbad05d6bd6fc2e1e515fe44da3eda
이민철 <lmc2819@naver.com>
2025-08-04 21:48:06 +0900

    [level 3] Title: 섬 연결하기, Time: 1.35 ms, Memory: 88.1 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
@@ -1,31 +1,35 @@
 import java.util.*;
+
 class Solution {
-    private int[] parent;
-    public int find(int a) {
-        if(parent[a] == a) return a;
-        else return parent[a] = find(parent[a]);
-    }
-    
-    public void union(int a, int b) {
-        a = find(a);
-        b = find(b);
-        if(a != b) {
-            parent[b] = a;
-        }
-    }
-    
     public int solution(int n, int[][] costs) {
         int answer = 0;
-        parent = new int[n];
-        for(int i = 0; i < n; i++) {
-            parent[i] = i;
+        
+        List<List<int[]>> adjList = new ArrayList<>();
+        for(int i = 0; i < n; i++) adjList.add(new ArrayList<>());
+        
+        for(int[] cost: costs){
+            int node1 = cost[0];
+            int node2 = cost[1];
+            
+            adjList.get(node1).add(new int[]{node2, cost[2]});
+            adjList.get(node2).add(new int[]{node1, cost[2]});
         }
-        Arrays.sort(costs, (o1, o2) -> o1[2] - o2[2]);
-        //Kruskal Algorithm
-        for(int i = 0; i < costs.length; i++) {
-            if(find(costs[i][0]) != find(costs[i][1])) {
-                union(costs[i][0], costs[i][1]);
-                answer += costs[i][2];
+        
+        boolean[] visited = new boolean[n];
+        
+        PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> o1[1]- o2[1]);
+        pq.offer(new int[]{0, 0});
+        
+        while(!pq.isEmpty()){
+            int[] node = pq.poll();
+            
+            if(visited[node[0]]) continue;
+            
+            visited[node[0]] = true;
+            answer += node[1];
+            
+            for(int[] next: adjList.get(node[0])){
+                pq.offer(next);
             }
         }
         return answer;
