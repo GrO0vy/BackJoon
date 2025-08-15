71bb6ea25d81fb331aaf87be510763129912c014
이민철 <lmc2819@naver.com>
2025-08-15 15:08:29 +0900

    [Gold IV] Title: 스카이라인 쉬운거, Time: 188 ms, Memory: 20892 KB -BaekjoonHub

--- 백준/Gold/1863. 스카이라인 쉬운거/스카이라인 쉬운거.java
+++ 백준/Gold/1863. 스카이라인 쉬운거/스카이라인 쉬운거.java
@@ -14,30 +14,22 @@ public class Main{
             buildings[i] = Integer.parseInt(input[1]);
         }
 
-        int result = buildings[0] > 0 ? 1 : 0;
+        int result = 0;
 
         Stack<Integer> stack = new Stack<>();
 
-        for(int i = 0; i < n - 1; i++){
-            if(buildings[i + 1] == 0){
+        for(int i = 0; i < n; i++){
+            if(buildings[i] == 0){
                 while(!stack.isEmpty()) stack.pop();
                 continue;
             }
 
-            if(buildings[i + 1] > buildings[i]){
+            while(!stack.isEmpty() && stack.peek() > buildings[i]) stack.pop();
+
+            if(stack.isEmpty() || stack.peek() != buildings[i]){
+                stack.push(buildings[i]);
                 result++;
             }
-            else {
-                while (!stack.isEmpty() && stack.peek() > buildings[i + 1]) {
-                    stack.pop();
-                }
-
-                if(stack.isEmpty() || stack.peek() != buildings[i + 1] && buildings[i + 1] > 0) {
-                    result++;
-                }
-            }
-
-            stack.push(buildings[i]);
         }
 
         System.out.println(result);
