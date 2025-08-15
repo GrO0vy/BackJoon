28af3e79a7fc32f462ac568696108dc81cbd7b84
이민철 <lmc2819@naver.com>
2025-08-15 12:33:59 +0900

    [Silver III] Title: 블로그, Time: 324 ms, Memory: 35100 KB -BaekjoonHub

--- 백준/Silver/21921. 블로그/블로그.java
+++ 백준/Silver/21921. 블로그/블로그.java
@@ -13,31 +13,22 @@ public class Main{
         st = new StringTokenizer(br.readLine());
 
         for(int i = 0; i < N; i++) visit[i] = Integer.parseInt(st.nextToken());
-
-        int left = 0;
-        int right = 0;
+        
+        
         int total = 0;
-        int max = -1;
-        int count = 0;
+        for(int i = 0; i < X; i++) total += visit[i];
+        
+        int max = total;
+        int count = 1;
 
-        while(left <= right && right < N){
-            if(right < X){
-                total += visit[right++];
-                max = total;
-                count = 1;
-                continue;
-            }
-
-            total -= visit[left++];
-            total += visit[right++];
-
+        for(int i = X; i < N; i++){
+            total += visit[i] - visit[i - X];
+            
             if(total > max){
                 max = total;
                 count = 1;
             }
-            else if(total == max){
-                count++;
-            }
+            else if(total == max) count++;
         }
 
         if(max == 0) System.out.println("SAD");
