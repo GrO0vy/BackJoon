625b171dbd9f21db69f6ed1fe30728a89938dfcc
이민철 <lmc2819@naver.com>
2025-08-26 09:29:44 +0900

    [Silver I] Title: 문자열 교환, Time: 104 ms, Memory: 14192 KB -BaekjoonHub

--- 백준/Silver/1522. 문자열 교환/문자열 교환.java
+++ 백준/Silver/1522. 문자열 교환/문자열 교환.java
@@ -8,37 +8,19 @@ public class Main {
 
         int n = str.length();
         int window = 0;
-
-        // 전체 a 개수 계산
+        
         for (int i = 0; i < n; i++) {
             if (str.charAt(i) == 'a') window++;
         }
+        
+        int result = Integer.MAX_VALUE;
 
-        // edge case: a가 하나도 없거나 모두 a이면 0번 교환
-        if (window == 0 || window == n) {
-            System.out.println(0);
-            return;
-        }
-
-        // 원형 처리 위해 2배
-        str += str;
-
-        int cntB = 0;
-
-        // 초기 윈도우 설정 (0 ~ window-1)
-        for (int i = 0; i < window; i++) {
-            if (str.charAt(i) == 'b') cntB++;
-        }
-
-        int result = cntB;
-
-        // 슬라이딩 윈도우 시작점 i = 1부터 시작
-        for (int i = 1; i < n; i++) {
-            // 왼쪽 문자 제거
-            if (str.charAt(i - 1) == 'b') cntB--;
-            // 오른쪽 문자 추가
-            if (str.charAt(i + window - 1) == 'b') cntB++;
-
+        for(int i = 0; i < n; i++){
+            int cntB = 0;
+            for(int j = i; j < i + window; j++){
+                if(str.charAt(j % n) == 'b') cntB++;    
+            }
+            
             result = Math.min(result, cntB);
         }
 
