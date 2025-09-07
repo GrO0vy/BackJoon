9a721a602c62f2f3510f7553b2b60d253cb5c1bf
이민철 <lmc2819@naver.com>
2025-09-07 18:25:59 +0900

    [level 3] Title: 광고 삽입, Time: 262.51 ms, Memory: 283 MB -BaekjoonHub

---
+++ 프로그래머스/3/72414. 광고 삽입/광고 삽입.java
@@ -0,0 +1,62 @@
+class Solution {
+    public String solution(String play_time, String adv_time, String[] logs) {
+        long[] timeLog = new long[360001];
+        
+        for(String log: logs){
+            String[] playTime = log.split("-");
+            int start = toInt(playTime[0]);
+            int end = toInt(playTime[1]);
+            
+            timeLog[start]++;
+            timeLog[end]--;
+        }
+        
+        for(int i = 1; i < timeLog.length; i++){
+            timeLog[i] += timeLog[i - 1];
+        }
+        
+        for (int i = 1; i < timeLog.length; i++) {
+            timeLog[i] += timeLog[i - 1];
+        }
+
+        
+        int total = toInt(play_time);
+        int adv = toInt(adv_time);
+        
+        long maxView = timeLog[adv - 1];
+        int maxStart = 0;
+        
+        for(int i = adv; i <= total; i++){
+            long view = timeLog[i] - timeLog[i - adv];
+            
+            if(view > maxView){
+                maxStart = i - adv + 1;
+                maxView = view;
+            }
+        }
+        
+        return toString(maxStart);
+    }
+    
+    int toInt(String timeStr){
+        String[] time = timeStr.split(":");
+        int hour = Integer.parseInt(time[0]);
+        int minute = Integer.parseInt(time[1]);
+        int second = Integer.parseInt(time[2]);
+        
+        return hour * 3600 + minute * 60 + second;
+    }
+    
+    String toString(int timeInt){
+        int hour = timeInt / 3600;
+        timeInt -= 3600 * hour;
+        
+        int second = timeInt % 60;
+        timeInt /= 60;
+        
+        int minute = timeInt % 60;
+        timeInt /= 60;
+        
+        return String.format("%02d:%02d:%02d", hour, minute, second);
+    }
+}
