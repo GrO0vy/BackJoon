31f243f92ece4915cc3fd15b7e54c0b4a63eb32e
이민철 <lmc2819@naver.com>
2025-09-19 11:30:23 +0900

    [Bronze I] Title: 이항 계수 1, Time: 104 ms, Memory: 14252 KB -BaekjoonHub

---
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int[][] dp = new int[N + 2][N + 2];
+
+        for(int i = 0; i <= N; i++){
+            dp[i][0] = 1;
+            for(int j = 1; j <= i; j++){
+                dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+            }
+        }
+
+        System.out.println(dp[N][K]);
+    }
+}
